fd95553a7d9523d680a6fe2a6a7ead8649bce733
MomPansy <jaydenyeo.acs@gmail.com>
2022-03-18 18:59:16 +0800

    Create roomtypes with their base price, and create a hotel class that instantiates 12 of each rooms.

---
+++ Rooms/vipSuiteRoom.java
@@ -0,0 +1,10 @@
+package Rooms;
+
+public class vipSuite extends Room {
+    public vipSuite(int roomNumber)
+    {
+        super(roomNumber);
+        this.roomNumber = roomNumber;
+        this.fare = 400;
+    }
+}
