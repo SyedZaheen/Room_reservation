66aeaa75685ca133914fb9d6ea4eda872c9846b2
SZaheen60 <zaheen60@gmail.com>
2022-04-09 11:52:30 +0800

    Updated DB file path+roomdb

--- HotelApp.java
+++ HotelApp.java
@@ -1,9 +1,14 @@
 
+import java.util.List;
+
 import com.controller.GuestControl;
+import com.db.roomDB.RoomDB;
+import com.models.Room;
 import com.utils.FrontendUtils;
 
 public class HotelApp {
     public static void main(String[] args) {
+
         // First thing: Show the main menu
         int choice = FrontendUtils.getUserChoice(new String[]{
             "Manage Guest",
--- com/db/roomDB/RoomDB.java
+++ com/db/roomDB/RoomDB.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.db.DB;
 import com.db.SerializeDB;
 import com.models.Room;
+import com.utils.AnonymousFunction;
 
 public class RoomDB implements DB<Room> {
 
@@ -13,7 +14,7 @@ public class RoomDB implements DB<Room> {
     // todo: Create entry doesnt make sense here
     @Override
     public boolean createEntry(Room room) {
-        
+
         return false;
     }
 
@@ -22,5 +23,19 @@ public class RoomDB implements DB<Room> {
         return SerializeDB.readSerializedObject(
                 DB.FILE_PATH + ROOM_DB_FILE_NAME);
     }
+
+
+    // public Room findRoomByVacancy(
+            
+    //         AnonymousFunction<Boolean, Room> validator
+            
+    //         ) {
+    //     List<Room> allEntries = findAllEntries();
+    //     for (Room room : allEntries) {
+    //         if (validator.execute(room))
+    //             return room;
+    //     }
+    //     return null;
+    // }
 
 }
