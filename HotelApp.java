8dcd6722ea289208c78aea23d5dd382ecb8d1c67
hochingwei <meredyddho@gmail.com>
2022-04-09 12:13:18 +0800

    resolved save conflicts

--- HotelApp.java
+++ HotelApp.java
@@ -1,35 +1,36 @@
-<<<<<<< HEAD
-=======
-
-import java.util.List;
-
->>>>>>> zaheen-edits
 import com.controller.GuestControl;
-import com.db.roomDB.RoomDB;
-import com.models.Room;
+import com.controller.ReservationControl;
 import com.utils.FrontendUtils;
 
 public class HotelApp {
     public static void main(String[] args) {
-
         // First thing: Show the main menu
-        int choice = FrontendUtils.getUserChoice(new String[]{
-            "Manage Guest",
-            "Manage Reservations",
-            "Manage Room Service",
-            "Manage Payment",
-            "Manage Rooms"
-        });
+        int choice = showMainMenuAndReturnChoice();
 
         // For each choice of the menu, execute what we want to do.
         switch (choice) {
             case 1:
                 new GuestControl().process();
                 break;
+
+            case 2:
+                new ReservationControl().process();
+                break;
+
             default:
                 break;
         }
 
+    }
+
+    public static int showMainMenuAndReturnChoice() {
+        return FrontendUtils.getUserChoice(new String[]{
+            "Manage Guest",
+            "Manage Reservations",
+            "Manage Room Service",
+            "Manage Payment",
+            "Manage Rooms"
+        });
     }
 
     
--- com/models/Room.java
+++ com/models/Room.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.models;
 
 import com.enums.RoomStatuses;
@@ -27,33 +26,3 @@ public class Room implements Model<Room> {
         return status;
     }
 }
-=======
-package com.models;
-
-import com.enums.RoomStatuses;
-import com.enums.RoomTypes;
-
-public class Room implements Model<Room> {
-
-    private RoomStatuses status;
-    private RoomTypes roomType;
-
-    Room(RoomStatuses status, RoomTypes roomType) {
-
-        this.status = status;
-        this.roomType = roomType;
-    }
-
-    public RoomTypes getRoomType() {
-        return roomType;
-    }
-
-    public void setStatus(RoomStatuses status) {
-        this.status = status;
-    }
-
-    public RoomStatuses getStatus() {
-        return status;
-    }
-}
->>>>>>> zaheen-edits
